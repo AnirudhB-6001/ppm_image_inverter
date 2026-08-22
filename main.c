2cd30c147586f541f4058a57a6385a7ab365b4bb
Anirudh Batra <hello@anirudhbatraofficial.com>
2026-08-22 13:39:21 +0530

    Invert RGB byte buffer

--- main.c
+++ main.c
@@ -13,10 +13,18 @@ int main(void)
     assert(invert_value(20) == 235);
     assert(invert_value(invert_value(20)) == 20);
 
-    int original = 20;
-    int inverted = invert_value(original);
+    unsigned char pixels[6] = {0, 0, 0, 255, 0, 0};
+    for (int i = 0; i < 6; i++)
+    {
+        pixels[i] = invert_value(pixels[i]);
+    }
 
-    printf("%d\n", inverted);
+    assert(pixels[0] == 255);
+    assert(pixels[1] == 255);
+    assert(pixels[2] == 255);
+    assert(pixels[3] == 0);
+    assert(pixels[4] == 255);
+    assert(pixels[5] == 255);
 
     return 0;
 }
