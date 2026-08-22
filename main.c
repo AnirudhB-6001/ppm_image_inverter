adb1f6b4db6a33e9d11287495c38b56d3c920874
Anirudh Batra <hello@anirudhbatraofficial.com>
2026-08-22 12:11:58 +0530

    Add tested byte inversion

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
+#include <assert.h>
 #include <stdio.h>
+
+int invert_value(int original)
+{
+    return 255 - original;
+}
 
 int main(void)
 {
-    printf("PPM inverter\n");
+    assert(invert_value(0) == 255);
+    assert(invert_value(255) == 0);
+    assert(invert_value(20) == 235);
+    assert(invert_value(invert_value(20)) == 20);
+
+    int original = 20;
+    int inverted = invert_value(original);
+
+    printf("%d\n", inverted);
+
     return 0;
 }
